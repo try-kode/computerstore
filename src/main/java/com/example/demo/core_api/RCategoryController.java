d7255ad31c2baa7484fccaa651b244c345def6c6
try-kode <leangtrysam656@gmail.com>
2025-09-16 00:02:03 +0700

    create api for category IV

--- src/main/java/com/example/demo/core_api/RCategoryController.java
+++ src/main/java/com/example/demo/core_api/RCategoryController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/category")
 public class RCategoryController {
@@ -24,8 +26,48 @@ public class RCategoryController {
         return ApiResponseStructure.singleResponse("Created", category, HttpStatus.CREATED);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<Object> getAll(){
+        List<Category> categories = categoryServiceImp.getAllCategories();
+        if (categories != null){
+            return ApiResponseStructure.singleResponse("Success", categories, HttpStatus.OK);
+        } else {
+            return ApiResponseStructure.singleResponse("Not found", null, HttpStatus.OK);
+        }
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Object> show(@PathVariable Long id){
-        return ApiResponseStructure.singleResponse("hello " + id, null, HttpStatus.OK);
+        Category category = categoryServiceImp.getCategoryById(id);
+        if (category != null){
+            return ApiResponseStructure.singleResponse("Success", category, HttpStatus.OK);
+        } else {
+            return ApiResponseStructure.singleResponse("Not found", null, HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody CategoryRequest categoryRequest){
+        Category category = categoryServiceImp.getCategoryById(id);
+        if (category != null){
+            category.setCategoryName(categoryRequest.getCategoryName());
+            category.setDescription(categoryRequest.getDescription());
+            categoryServiceImp.updateCategoryById(id, category);
+            return ApiResponseStructure.singleResponse("Updated", category, HttpStatus.CREATED);
+        } else {
+            return ApiResponseStructure.singleResponse("Not found", null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id){
+        Category category = categoryServiceImp.getCategoryById(id);
+        if (category != null){
+            categoryServiceImp.deleteCategoryById(id);
+            return ApiResponseStructure.singleResponse("Deleted", category, HttpStatus.OK);
+        } else {
+            return ApiResponseStructure.singleResponse("Not found", null, HttpStatus.NOT_FOUND);
+        }
     }
 }
