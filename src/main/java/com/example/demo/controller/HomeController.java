bea7d14c93fd112834a441aed5937dfa35e7a96f
try-kode <leangtrysam656@gmail.com>
2025-09-16 01:03:08 +0700

    create api for category VI

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String index(){
         return "App is running";
     }
