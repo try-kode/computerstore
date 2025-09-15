38d2fc28ae07a4ba6d122a93ca4f9aaa71dd96b1
try-kode <leangtrysam656@gmail.com>
2025-09-15 23:41:15 +0700

    create api for category II

--- src/main/java/com/example/demo/core_api/RCategoryController.java
+++ src/main/java/com/example/demo/core_api/RCategoryController.java
@@ -6,10 +6,7 @@ import com.example.demo.serviceImp.CategoryServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -25,5 +22,10 @@ public class RCategoryController {
         category.setDescription(categoryRequest.getDescription());
         categoryServiceImp.createCategory(category);
         return ApiResponseStructure.singleResponse("Created", category, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> show(@PathVariable Long id){
+        return ApiResponseStructure.singleResponse("hello " + id, null, HttpStatus.OK);
     }
 }
