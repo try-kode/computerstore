8a6a5b22fa89b0e473ff9226cff818e20b101023
try-kode <leangtrysam656@gmail.com>
2025-09-15 22:44:18 +0700

    create category model

---
+++ src/main/java/com/example/demo/controller/model/Category.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String uuid;
+    private String categoryName;
+    private String description;
+    private String status;
+    private String publisher;
+    private Date publishedDate;
+    private Date modifiedDate;
+
+    public Category() {
+        this.status = "1";
+        this.publisher = "s.admin";
+        this.publishedDate = new Date();
+        this.modifiedDate = new Date();
+    }
+}
